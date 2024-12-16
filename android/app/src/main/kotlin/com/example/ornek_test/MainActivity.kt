e8ee47ed9799df5e54d46e57a52f47a38c66d18e
bilalkarasin <134536309+bilalkarasin@users.noreply.github.com>
2024-12-16 16:45:02 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/ornek_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ornek_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/rw/ornek_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rw.ornek_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
